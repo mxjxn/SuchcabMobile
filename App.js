f238258f07bb994b59c6f2a0f3fb239aeedaa8be
MXJXN <modulations@gmail.com>
2020-04-06 16:24:42 -0400

    added navigation stack and temp images

--- App.js
+++ App.js
@@ -3,6 +3,46 @@ import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 import styled from 'styled-components/native';
 import { StyleSheet, Image, Text, View, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function LoginScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+        <Image source={require('./assets/images/callig1.png')}
+               style={styles.stretch} />
+        <Text
+          style={{fontFamily: 'PermanentMarker', fontSize: 60}}
+        >such.cab</Text>
+        <Text
+          style={{fontFamily: 'Lato-Regular', fontSize: 20}}
+        >
+            Rides and tours and stuff.
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Dash')
+          }}
+          style={styles.button}>
+            <Text
+              style={styles.buttonText}>
+                Log In or Sign Up!
+            </Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+function DashboardScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Dashboard</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -14,26 +54,12 @@ export default function App() {
     return <AppLoading />
   } else {
     return (
-      <View style={styles.container}>
-          <Image source={require('./assets/images/callig1.png')}
-            style={styles.stretch} />
-          <Text
-            style={{fontFamily: 'PermanentMarker', fontSize: 60}}
-            >such.cab</Text>
-          <Text
-            style={{fontFamily: 'Lato-Regular', fontSize: 20}}
-          >
-              Rides and tours and stuff.
-          </Text>
-          <TouchableOpacity
-            onPress={() => alert('You\'re cute')}
-            style={styles.button}>
-              <Text
-                style={styles.buttonText}>
-                  Log In or Sign Up!
-              </Text>
-          </TouchableOpacity>
-      </View>
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Dash" component={DashboardScreen} />
+          </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
